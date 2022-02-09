535884ad547fb4eb8c9cee00cc1a14a9b6a12572
Marcos Volpi <marcos@marcosvolpi.com>
2022-02-09 18:37:01 -0300

    Add Action Cable implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { createConsumer } from "@rails/actioncable"
 
 function App() {
+  const consumer = createConsumer('http://127.0.0.1:3000/cable');
+  //consumer.subscriptions.create({ channel: "ChatChannel", room: "Best Room" })
+
+  const chatChannel = consumer.subscriptions.create({ channel: "ChatChannel", room: "Best Room" }, {
+    received(data) {
+      console.log('Recived: ', data["sent_by"], data["body"])
+    }
+  });
+
+  function send(){
+    chatChannel.send({ sent_by: "Paul", body: "This is a cool chat app." });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <button onClick={() => send()}>Send message</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
