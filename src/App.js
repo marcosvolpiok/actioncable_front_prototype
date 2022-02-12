932b7df1d968ea13cbdddc27e4707f85ffa78da4
Marcos Volpi <marcos@marcosvolpi.com>
2022-02-11 23:50:14 -0300

    Add multiple users functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { createConsumer } from "@rails/actioncable";
 import Peer from 'peerjs';
-const myPeer = new Peer(undefined,
+var myPeer = new Peer(undefined,
   {
     host: '/',
     port: 3001
@@ -12,25 +12,26 @@ const myPeer = new Peer(undefined,
 
 
 
+
+
 function App() {
   //var ownVideo = document.getElementById('ownVideo');
   useEffect(()=>{
     console.log('construct');
-    var ownVideo = document.createElement('video')
     var grid = document.getElementById('grid');
     console.log('griddd', grid)
     const peers = {};
-    let ownVideoSrc = "";
   
 
     const myVideo = document.createElement('video');
     myVideo.muted = true;
 
+
     navigator.mediaDevices.getUserMedia({
       video: true,
       audio: true
     }).then(stream => {
-      console.log('addVideoStream 1111')
+      console.log('addVideoStream 1111', stream)
       addVideoStream(myVideo, stream)  
       myPeer.on('call', call => {
         call.answer(call.stream)
@@ -39,8 +40,68 @@ function App() {
           addVideoStream(video, userVideoStream)
         })
       })
+
+ 
+     
     });
+
+
+
   
+
+    const consumer = createConsumer('http://127.0.0.1:3000/cable');
+    //consumer.subscriptions.create({ channel: "ChatChannel", room: "Best Room" })
+  
+    const chatChannel = consumer.subscriptions.create({ channel: "ChatChannel", room: "Best Room" }, {
+      received(data) {
+        console.log('data received', data)
+        //console.log('Recived: ', data["sent_by"], data["body"])
+        if(data["body"]=="join-room"){
+          navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+          }).then(stream => {
+            connectToNewUser(data["sent_by"], stream);
+          });
+        }
+  
+  
+        function connectToNewUser(userId, stream) {
+          console.log('new user - connect', myPeer);
+
+          const call = myPeer.call(userId, stream)
+          const video = document.createElement('video')
+          call.on('stream', userVideoStream => {
+            console.log('llegó a stream')
+            addVideoStream(video, userVideoStream)
+          })
+          call.on('close', () => {
+            video.remove()
+          })
+        
+          peers[userId] = call
+        }      
+  
+        //   socket.on('user-connected', userId => {
+        //     connectToNewUser(userId, stream)
+        //   })
+        // })
+        
+        // socket.on('user-disconnected', userId => {
+        //   if (peers[userId]) peers[userId].close()
+        // })      
+      }
+    });    
+
+    myPeer.on('open', id => {
+      console.log('open')
+      chatChannel.send({ sent_by: id, body: "join-room"});
+      //chatChannel.send({ action: "join-room", id: id});
+      //chatChannel.send({ sent_by: "Paul", body: "This is a cool chat app." });
+    })
+
+
+
     function addVideoStream(video, stream) {
       console.log('videooooooooo', video);
       video.srcObject = stream
@@ -51,38 +112,20 @@ function App() {
         //video.play()
       })
       grid.append(video)
-    }  
-    
-    
-    function connectToNewUser(userId, stream) {
-      const call = myPeer.call(userId, stream)
-      const video = document.createElement('video')
-      call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
-      })
-      call.on('close', () => {
-        video.remove()
-      })
-    
-      peers[userId] = call
-    }    
+    }      
+
+
+
   });
 
 
 
 
 
-  const consumer = createConsumer('http://127.0.0.1:3000/cable');
-  //consumer.subscriptions.create({ channel: "ChatChannel", room: "Best Room" })
 
-  const chatChannel = consumer.subscriptions.create({ channel: "ChatChannel", room: "Best Room" }, {
-    received(data) {
-      console.log('Recived: ', data["sent_by"], data["body"])
-    }
-  });
 
   function send(){
-    chatChannel.send({ sent_by: "Paul", body: "This is a cool chat app." });
+    //chatChannel.send({ sent_by: "Paul", body: "This is a cool chat app." });
   }
 
   function handleClick(){
